8082f91f70203547f9d8c7eea33a70b1ecf0abe4
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-06-01 02:02:44 +0530

    add fireworks effect for celebration

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,47 @@
 const participants = document.querySelector("textarea");
 const selectBtn = document.querySelector("button");
 const winnerDiv = document.querySelector("#winner");
+const winnerInfo = document.querySelector("#info");
 
-selectBtn.addEventListener("click", () => {
+selectBtn.addEventListener("click", startChoosing);
+
+let winnerUsername;
+
+function selectRandomUser(prefix, participantsList, totalParticipants) {
+  {
+    let winner = Math.floor(Math.random() * totalParticipants);
+    winnerUsername = participantsList[winner];
+    winnerDiv.innerHTML =
+      prefix + "<span class='pink winner'>@" + winnerUsername + "</span>";
+  }
+}
+
+function showWinner(winner) {
+  alert(winner);
+}
+
+function randomRepeater(participantsList, totalParticipants) {
+  let i = 1;
+  let count = 10;
+  winnerInfo.style.display = "flex";
+  const interval = setInterval(() => {
+    selectRandomUser("choosing... ", participantsList, totalParticipants);
+    if (i % 10 == 0) count--;
+    winnerInfo.innerHTML = `<span class="num">${count}</span>`;
+    i++;
+    if (i > 100) {
+      selectRandomUser("winner: ", participantsList, totalParticipants);
+      winnerInfo.innerHTML = `congrats<span class="info-winner">@${winnerUsername}</span>`;
+
+      $("#container").fireworks();
+
+      clearInterval(interval);
+    }
+  }, 100);
+}
+
+function startChoosing() {
   let participantsList = participants.value.split("\n");
   let totalParticipants = participantsList.length;
-  let winner = Math.floor(Math.random() * totalParticipants);
-
-  winnerDiv.innerHTML =
-    "winner: <span class='pink winner'>@" +
-    participantsList[winner] +
-    "</span>";
-});
+  randomRepeater(participantsList, totalParticipants);
+}
